cd6b45e505e092b9aa6900885836d4e890946382
Levi <uuoo10levi@gmail.com>
2024-11-01 15:55:25 -0500

    Stable!!!

--- contract.py
+++ contract.py
@@ -3,19 +3,20 @@ from reportlab.lib.pagesizes import letter, landscape, inch
 from reportlab.platypus import Paragraph, SimpleDocTemplate, Table
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib import styles
+from reportlab.pdfgen.canvas import Canvas
 
 import json
 
-def comparelist (lst1, lst2):
-    counter = 0
-    for i in [0, 1, 2]:
-        if lst1[i] == lst2[i]:
-            counter += 1
+# def comparelist (lst1, lst2):
+#     counter = 0
+#     for i in [0, 1, 2]:
+#         if lst1[i] == lst2[i]:
+#             counter += 1
             
-    if counter == 3:
-        return True
-    else:
-        return False
+#     if counter == 3:
+#         return True
+#     else:
+#         return False
     
 def itemnumberfromlist (lst):
     return str(lst[0]) + lst[1] + lst[2]
@@ -36,8 +37,8 @@ class PanelObjects:
     
     
 class Items:
-    def __init__(self, itemno='', description='', count=1, names=[]):
-        self.count = count
+    def __init__(self, itemno='', description='', pnltotal=1, names=[]):
+        self.pnltotal = pnltotal
         self.names = names
         self.itemno = itemno
         self.description = description
@@ -55,21 +56,84 @@ class Items:
         self.names.append(name)
         self.count = len(self.names)
 
+class NumberedPageCanvas(Canvas):
+    """
+    http://code.activestate.com/recipes/546511-page-x-of-y-with-reportlab/
+    http://code.activestate.com/recipes/576832/
+    http://www.blog.pythonlibrary.org/2013/08/12/reportlab-how-to-add-page-numbers/
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Constructor"""
+        super().__init__(*args, **kwargs)
+        self.pages = []
+
+    def showPage(self):
+        """
+        On a page break, add information to the list
+        """
+        self.pages.append(dict(self.__dict__))
+        self._startPage()
+
+    def save(self):
+        """
+        Add the page number to each page (page x of y)
+        """
+        page_count = len(self.pages)
+
+        for page in self.pages:
+            self.__dict__.update(page)
+            self.draw_page_number(page_count)
+            self.draw_rev_number()
+            super().showPage()
+
+        super().save()
+
+    def draw_page_number(self, page_count):
+        """
+        Add the page number
+        """
+        page = "Page %s of %s" % (self._pageNumber, page_count)
+        self.setFont("Helvetica", 8)
+        self.drawRightString(10.75 * inch, 0.25 * inch, page)
+        
+    def draw_rev_number(self):
+        self.setFont("Helvetica", 8)
+        self.drawString(0.25 * inch, 0.25 * inch, 'Rev. 0')
+        
 class pdf:
     def __init__(self, data, name = '_.pdf'):
         
         self.styleSheet = getSampleStyleSheet()
         self.pagesize = (11 * inch, 8.5 * inch)  # 20 inch width and 10 inch height.doc = SimpleDocTemplate('sample.pdf', pagesize=pagesize)
-        self.customstyle1 = ParagraphStyle(name='BodyText', parent=self.styleSheet['BodyText'], spaceBefore=6, alignment=1)
+        self.styleCustomCenterJustified = ParagraphStyle(name='BodyText', parent=self.styleSheet['BodyText'], spaceBefore=6, alignment=1, fontSize=8)
+        self.styleCustomLeftJustified = ParagraphStyle(name='BodyText', parent=self.styleSheet['BodyText'], spaceBefore=6, alignment=0, fontSize=8)
         self.doc = SimpleDocTemplate(name, pagesize=self.pagesize)
+        self.doc.__setattr__('topMargin', 0.25*inch)
+        self.doc.__setattr__('leftMargin', 0.25*inch)
+        self.doc.__setattr__('rightMargin', 0.25*inch)
+        self.doc.__setattr__('bottomMargin', 0.25*inch)
         self.elements = []
-
-        for i in range(len(data[0])):
-            data[0][i] = Paragraph(data[0][i], self.customstyle1)
         
-        self.pdftable = Table(data, colWidths= 100, style=[
-            ('BOX',(0,0),(-1,-1),1,colors.black),
+
+        for i in range(len(data)):
+            for j in range(len(data[i])):
+                if j != 1:
+                    data[i][j] = Paragraph(str(data[i][j]), self.styleCustomCenterJustified)
+                
+        for i in range(len(data)):
+            if i > 2:
+                data[i][1] = Paragraph(str(data[i][1]), self.styleCustomLeftJustified)
+            
+        # data[2][1] = Paragraph(str(data[2][1]), self.styleCenterJustified)
+        # data[2][1] = Paragraph(str(data[2][1]), self.styleCenterJustified)
+                
+        
+        self.pdftable = Table(data, colWidths= (50,200,50,100,100,100), repeatRows=3, style=[
             ('GRID',(0,0),(-1,-1),0.5,colors.black),
+            ('SPAN', (0,0), (-1, 0)),
+            ('SPAN', (0,1), (1, 1)),
+            ('SPAN', (2,1), (-1, 1)),
             ('ALIGN',(0,0),(-1,-1),'CENTER'),
             ('VALIGN',(0,0),(-1,-1),'TOP')]
                               )
@@ -77,149 +141,72 @@ class pdf:
         self.elements.append(self.pdftable)
 
     def exportPDF(self):
-        self.doc.build(self.elements)
+        self.doc.build(self.elements, canvasmaker=NumberedPageCanvas)
         
 
-with open('contract.json', 'r') as file:
-    contract = json.load(file)
+def Main(contract=''):
+    with open('contract.json', 'r') as file:
+        contract = json.load(file)
 
-panelList = [PanelObjects(i,name=name) for i, name in enumerate(contract)]
-
-
-for i,obj in enumerate(panelList):
-    obj.description = contract[obj.name]['description']
+    panelList = [PanelObjects(i,name=name) for i, name in enumerate(contract)]
 
 
-def filterdesc(var):
-    filters = ['description']
-    if (var not in filters):
-        return True
-    else:
-        return False
-    
-projectList = []
+    for i,obj in enumerate(panelList):
+        obj.description = contract[obj.name]['description']
 
-# for i, panel in enumerate(panelList):
-#     # print(panelList[i].keys())
-#     # print(contract[panel.name].items())
-#     for i, item in enumerate(contract[panel.name]):
-#         panelitemlist = [item]
-#         if item != 'description':
-#             panelitemlist.append(contract[panel.name][item])
-#         if item != 'description':
-#             projectList.append(panelitemlist)
 
-print(panelList)
+    for j, pnlobj in enumerate(panelList):
+        pnlname = pnlobj.name
+        itemList = [Items(itemno=i,description=contract[pnlname][i]['description'],pnltotal=contract[pnlname][i]['count'],names=contract[pnlname][i]['names']) for i in contract[pnlname].keys() if i not in ('description')]
+        panelList[j].items = itemList
 
-for j, pnlobj in enumerate(panelList):
-    pnlname = pnlobj.name
-    itemList = [Items(itemno=i,description=contract[pnlname][i]['description'],count=contract[pnlname][i]['count'],names=contract[pnlname][i]['names']) for i in contract[pnlname].keys() if i not in ('description')]
-    panelList[j].items = itemList
+
+    headers = ['Item Number', 'Description', 'Total']
+
+    for i in range(len(panelList)):
+        headers.append(panelList[i].name)
+
+    title = ['SHOAL CREEK RELAY EQUPIMENT\nMATERIAL LIST']
+    subtitle = ['', '', 'QUANTITY/DEVICE NAME']
+    headers = ['Item Number', 'Description', 'Total']
         
-'''from previous file'''
-# for j in panelList:
-#         itemList = [Items(i.split('-'),names=contract['material'][j.id][i]['names'],description=contract['material'][j.id][i]['description']) for i in contract['material'][j.id].keys()]
-#         for i in range(len(itemList)):
-#             itemList[i].count = len(itemList[i].names)
-#         panelList[int(j.id)].items = itemList
-        
-# itemObjList = []
+    grid = [[0 for j in range(len(panelList)+3)] for i in range(len(panelList)+2)]
 
-# for j, panelobj in enumerate(panelList):
-#     for i, key in enumerate(contract[panelobj.name].keys()):
-#         if key not in ['description']:
-#             panelobj.names = contract[panelobj.name][key]
-#             itemObjList.append(Items(itemno=key,names=item['names'],description=item['description'],count=item['count'],))
-    # itemList = [Items(i,names=contract[panelobj.name][i]['names']) for i in filter(filterdesc, contract[panelobj.name].keys())]
-
-
-# itemList = [Items(i,names=contract[panelobj.name])]
-
-# print(itemList)
-
-# print(itemList[0].names)
-# print(itemList[1].names)
-    
-# # for i in itemList:
-# #     print(i)
-    
-# for j in panelList:
-#     itemList = [Items(i.split('-'),names=contract['material'][j.id][i]) for i in contract['material'][j.id].keys()]
-#     for i in range(len(itemList)):
-#         itemList[i].count = len(itemList[i].names)
-#     panelList[int(j.id)].items = itemList
-
-
-# headers = ['Item Number', 'Description', 'Total']
-
-# for i in range(len(panelList)):
-#     headers.append(panelList[i].name)
-
-# masterList = []
-    
-# for i in panelList:
-#     print(i)
-#     for j in i.items:
-#         print(j)
-#         if j.itemno not in masterList:
-#             masterList.append(j.itemno)
+    for i in range(len(panelList)+2):
+        title.append("")
             
-# print(masterList)
-
-# # # masterList.sort()
-# title = ['SHOAL CREEK RELAY EQUPIMENT<br/>MATERIAL LIST']
-# subtitle = ['', '', 'QUANTITY/DEVICE NAME']
-# headers = ['Item Number', 'Description', 'Total']
-    
-# grid = [[0 for j in range(len(panelList)+3)] for i in range(len(itemObjList)+2)]
-
-# # print(grid)
-
-# for i in range(len(panelList)+2):
-#     title.append("")
+    for i in range(len(panelList)):
+        subtitle.append("")
         
-# for i in range(len(panelList)):
-#     subtitle.append("")
+    for i in range(len(panelList)):
+        headers.append(panelList[i].name + '<br/>' + panelList[i].description) 
+
+    # print(headers)
+
+    grid[0] = title
+    grid[1] = subtitle
+    grid[2] = headers
+
+    allheaders = [title, subtitle, headers]
     
-# for i in range(len(panelList)):
-#     headers.append(panelList[i].name + '<br/>' + panelList[i].description) 
-
-# # print(headers)
-
-# grid[0] = title
-# grid[1] = subtitle
-# grid[2] = headers
-
-# itemList = [[item.itemno for i, item in enumerate(itemObjList)] for j in item.keys()]
-
-# # print(itemList)
-# # for i, item in enumerate(itemList):
-# #     print(item.itemno)
-# #     for j, keys in enumerate(item.keys()):
-# #         print(keys)
-# #     for j, item in enumerate(panel.name)
-    
-
-# for panel in range(len(panelList)):
-    
-#     for item in range(len(panelList[panel].items)):
-#         print(panelList[panel].items)
-        
-#         for item2 in range(len(masterList)):
-#             pass
-            
-# #             if comparelist(panelList[panel].items[item].itemtype, masterList[item2]):
-# #                 grid[item2+1][0] = str(masterList[item2][0]) + ''.join(masterList[item2][1:2])
-# #                 grid[item2+1][panel+3] = str(len(panelList[panel].items[item].names['names'])) + '\n' + '\n'.join(panelList[panel].items[item].names['names'])
-
-# # # for row in range(len(grid)):
-# # #     count = 0
-# # #     for column in range(2,len(grid[row])):
-# # #         print(grid[row][column])
-# # #         count += int(str(grid[row][column]).split('\n')[0])
-# # #         grid[row][2] = count
+    for i, panel in enumerate(panelList):
+        for j, items in enumerate(panel.items):
+            '''Fill Item Number'''
+            grid[j+len(allheaders)][0] = items.itemno
+            '''Fill description'''
+            grid[j+len(allheaders)][1] = items.description
+            '''Fill Item Count and Names in Each Panel Column'''
+            grid[j+len(allheaders)][i+3] = items.pnltotal + '<br/>' + '<br/>'.join(items.names)
                     
-# # # grid[0] = headers
+    '''Fill 'total count' column'''
+    for row in range(len(allheaders),len(grid)):
+        count = 0
+        for column in range(2,len(grid[row])):        
+            count += int(str(grid[row][column]).split('<br/')[0])
+            grid[row][2] = count
 
-# # # matList = pdf(grid, 'TST.pdf')
-# # # matList.exportPDF()
+    matList = pdf(grid, 'TST.pdf')
+    matList.exportPDF()
+
+if __name__ == '__main__':
+    Main()
